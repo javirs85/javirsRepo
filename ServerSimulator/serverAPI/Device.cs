c547c92b0a30f3acfa60ee84775b149cee48a19a
javirs85 <javirs@gmail.com>
2018-06-22 14:44:31 +0200

    added

---
+++ ServerSimulator/serverAPI/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameBrainTools
+{
+    class Device
+    {
+        EndPoint address;
+        int ID;
+
+        string name;
+    }
+}
