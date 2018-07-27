34edf2d62254af74f9a8d505ba0b7b24e6d2b729
javirs@gmail.com <Iluvat4r>
2018-07-27 19:45:31 +0200

    last minute update

--- gameBrain/gameBrain/WebSocketController.cs
+++ gameBrain/gameBrain/WebSocketController.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Networking.Sockets;
 
 namespace gameBrain
 {
     class WebSocketController
     {
+        MessageWebSocket server 
+
+        public WebSocketController()
+        {
+            var server = new MessageWebSocket();
+           
+        }
+
+        public async void Connect()
+        {
+            await server.ConnectAsync(new Uri("wss://www.contoso.com/mywebservice"));
+        }
     }
 }
