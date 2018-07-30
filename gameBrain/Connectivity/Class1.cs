3959693a880dcb248711d355e097f23fd69a9766
javirs@gmail.com <Iluvat4r>
2018-07-30 18:15:16 +0200

    added connectivity project as dll. Uses iotweb nuget package

---
+++ gameBrain/Connectivity/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IotWeb.Server;
+using IotWeb.Common.Http;
+using IotWeb.Common.Util;
+
+namespace WebServerApp
+{
+    class WebController
+    {
+        HttpServer webServer;
+        public static List<WebSocket> Sockets;
+        public static event EventHandler<string> newDebugMessageFromSuperServer;
+        public static event EventHandler<string> newMessageFromSocket;
+
+        public void StartAll()
+        {
+            Sockets = new List<WebSocket>();
+            webServer = new HttpServer(8000);
+            webServer.AddHttpRequestHandler(
+                "/",
+                new HttpResourceHandler(
+                    Utilities.GetContainingAssembly(typeof(WebController)),
+                    "Resources.Site",
+                    "index.html"
+                )
+            );
+            webServer.AddWebSocketRequestHandler(
+                "/sockets/",
+                new WebSocketHandler()
+            );
+
+            webServer.Start();
+        }
+
+        public void Send(string msg)
+        {
+            foreach (var socket in Sockets)
+                socket.Send(msg);
+        }
+
+        public static void OnDebugMessage(WebSocket sender, string str)
+        {
+            newDebugMessageFromSuperServer?.Invoke(sender, str);
+        }
+        public static void OnMessageFromSocket(WebSocket sender, string str)
+        {
+            newMessageFromSocket?.Invoke(sender, str);
+        }
+
+        public void Stop() { webServer.Stop(); }
+    }
+
+    class WebSocketHandler : IWebSocketRequestHandler
+    {
+        public void Connected(WebSocket socket)
+        {
+            WebController.Sockets.Add(socket);
+            socket.DataReceived += Socket_DataReceived;
+            WebController.OnDebugMessage(socket, "Socket connected");
+        }
+
+        private void Socket_DataReceived(WebSocket socket, string frame)
+        {
+            WebController.OnMessageFromSocket(socket, frame);
+        }
+
+        public bool WillAcceptRequest(string uri, string protocol)
+        {
+            return (uri.Length == 0) && (protocol == "brain");
+        }
+    }
+}
