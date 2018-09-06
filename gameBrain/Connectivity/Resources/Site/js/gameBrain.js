c5c5d2f7455b253269fa6190c208f0eac73aad8d
javirs85 <javirs@gmail.com>
2018-09-06 11:37:12 +0200

    webpage is super cool now

--- gameBrain/Connectivity/Resources/Site/js/gameBrain.js
+++ gameBrain/Connectivity/Resources/Site/js/gameBrain.js
@@ -1,27 +1,60 @@
 $(document).ready(function () {
+	var socket;
+	var isSocketConnected = false;
+	var wsURL;
+	
     if ("WebSocket" in window) {
-        var wsURL = "ws://" + location.hostname + (location.port ? ':' + location.port : '') + "/sockets/";
-        var socket = new WebSocket(wsURL, ['brain']);
+		try {
+			wsURL = "ws://" + location.hostname + (location.port ? ':' + location.port : '') + "/sockets/";
+			appendMsg("Trying to connect to "+wsURL);
+			socket = new WebSocket(wsURL, ['brain']);
+		} catch (err)
+		{
+			appendMsg(err);
+		}
     }
     else {
         alert("browser does not support websockets");
     }
 
     socket.onopen = function () {
-        //appendMsg("socket connected");
+		isSocketConnected = true;
+        appendMsg("socket connected");
     };
 
     socket.onmessage = function (e) {
         appendMsg(e.data);
     };
+	
+	socket.onerror = function (evt) {		
+		appendError("WebSocket error");	
+	}
+	socket.onclose = function (evt) {
+		if(evt.code == 1006)
+			appendError("Cannot connect to WebSocket server at<br/>"+wsURL);
+		else
+			appendMsg("Error Code: "+evt.code);
+	}
 
     $("#sendButton").click(function () {
-        socket.send("discoveryRequest");
+		if(socket.readyState == 0) appendMsg("Server is still CONNECTING");
+		else if (socket.readyState == 1)
+			socket.send("discoveryRequest");
+		else if (socket.readyState == 2)
+			appendError("The connection is in the process of closing.");
+		else if (socket.readyState == 3)
+			appendError("The connection is closed or couldn't be opened.");
+			
+		
     });
 
     function appendMsg(msg) {
         $("#messages").append("<li>" + msg + "</li>");
     }
+	
+	function appendError(msg){
+		$("#messages").append("<li class='error'>" + msg + "</li>");
+	}
 
     function render() {
 
