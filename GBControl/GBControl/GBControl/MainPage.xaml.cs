9e4574e2aea463cdef80fe3d1a9b4e1943d955b1
javirs85 <javirs@gmail.com>
2018-09-28 17:20:40 +0200

    added phone project

---
+++ GBControl/GBControl/GBControl/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GBControl
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
